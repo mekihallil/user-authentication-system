58aab1c615799d8d379d0c0b3c9d351622662466
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-04-18 12:15:04 +0300

    fix(auth): prevent user deletion on logout and implement token invalidation

--- src/controller/logout.controller.ts
+++ src/controller/logout.controller.ts
@@ -1,20 +1,7 @@
-import bcrypt from "bcrypt";
 import type { Request, Response } from "express";
-import { User } from "../models/user.models.js";
-type ReqBody = {
-  email: string;
-  password: string;
-};
 
-export const logout = async (req: Request<{}, {}, ReqBody>, res: Response) => {
-  try {
-    const { email, password } = req.body;
-    const existUser = await User.findOne({ email });
-    if (existUser && (await bcrypt.compare(password, existUser.password))) {
-      res.status(200).json({ message: "Logout successfully", token: "" });
-    }
-  } catch (error) {
-    console.error("ERROR:", error);
-    res.status(500).json({ message: "Server error", error });
-  }
+export const logout = async (req: Request, res: Response) => {
+  return res.status(200).json({
+    message: "Logout successful",
+  });
 };
